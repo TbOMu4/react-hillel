b2158a394d34936aa7f357b9d9cd4986b483cf4d
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-13 21:02:34 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/homework-25.1/",
+  base: "/react-hillel/",
   plugins: [react()],
 });
